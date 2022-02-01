03c9e1f86747e8fea00c174ca9de214d8451efe1
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-02-01 20:29:58 +0500

    Создание struct и enum

--- src/main_old.cpp
+++ src/main_old.cpp
@@ -16,80 +16,99 @@ enum type_room {
   living_room     // Гостиная
 };
 
-enum type_building {
-  home,    // Дом
-  garage,  // Гараж
-  shed,    // Сарай
-  bath     // Баня
-};
-
 struct Room {
-  type_room type_room;  // Тип комнат
-  double square;        // Площадь комнаты
-};
-
-struct Floor {
-  std::vector<Room> rooms;  // Вектор комнат
-  double ceiling_height;    // Высота потолков
+  std::vector<type_room> type_room;  // Вектор комнат
+  std::vector<double> square;        // Площадь комнаты
 };
 
 struct Home {
-  std::vector<Floor> floor;
+  struct Room room;
+  double ceiling_height;  // Высота потолков
+  bool bake;
 };
 
 struct Region {
-  type_building type_building;
-
-  // bool house = true;    // Дом
-  // bool garage = false;  // Гараж
-  // bool shed = false;    // Сарай
-  // bool bath = false;    // Баня
-  bool bath_bake = false;
-  bool home_bake = false;
+  struct Home home;
+  bool house = true;    // Дом
+  bool garage = false;  // Гараж
+  bool shed = false;    // Сарай
+  bool bath = false;    // Баня
 };
 
 int main() {
-  int countRegion;
-  int countFloor;
-  std::vector<Region> region;
-  Region composition_region;
   std::cout << "Model village" << std::endl;
-  std::cout << "Enter the number of areas" << std::endl;
-  std::cin >> countRegion;
+  Region region;
+  Home home;
   std::string setRoom;
   std::string setBake;
   std::string setGarage;
   std::string setShed;
   std::string setBath;
+  int countRegion;
+  int countRoom;
+  int countFloor;
+  double setSquare;
+
+  std::cout << "How many plots are there in the village?" << std::endl;
+  std::cin >> countRegion;
   for (int i = 0; i < countRegion; i++) {
-    std::cout << "There is garage (y/n)?" << std::endl;
+    std::cout << "Enter count floor in house" << std::endl;
+    std::cin >> countFloor;
+    if (countFloor > 0 && countFloor < 4) {
+      for (int j = 1; j <= countFloor; j++) {
+        std::cout << "Enter ceiling height [" << j << "] floor" << std::endl;
+        std::cin >> region.home.ceiling_height;
+        std::cout << "Count room" << std::endl;
+        std::cin >> countRoom;
+        if (countRoom > 1 && countRoom < 5) {
+          for (int k = 1; k <= countRoom; k++) {
+            std::cout << "Enter room" << std::endl;
+            std::cin >> setRoom;
+            region.home.room.type_room[k] = setRoom;
+            std::cout << "Enter square room" << std::endl;
+            std::cin >> setSquare;
+            region.home.room.square.push_back(setSquare);
+          }
+          std::cout << "There is a stove in the house? y/n" << std::endl;
+          std::cin >> setBake;
+          if (setBake == "y") {
+            region.home.bake = true;
+          } else {
+            region.home.bake = false;
+          }
+        } else {
+          std::cout << "Invalid value rooms" << std::endl;
+        }
+      }
+    } else {
+      std::cout << "Invalid value floors" << std::endl;
+    }
+
+    std::cout << "Is there a garage on the plot? y/n" << std::endl;
     std::cin >> setGarage;
     if (setGarage == "y") {
-      composition_region.garage = true;
+      region.garage = true;
+    } else {
+      region.garage = false;
     }
-    std::cout << "There is shed (y/n)?" << std::endl;
+    std::cout << "Is there a shed on the plot? y/n" << std::endl;
     std::cin >> setShed;
     if (setShed == "y") {
-      composition_region.shed = true;
+      region.shed = true;
+    } else {
+      region.shed = false;
     }
-    std::cout << "There is bath (y/n)?" << std::endl;
+    std::cout << "Is there a sauna on the site? y/n" << std::endl;
     std::cin >> setBath;
     if (setBath == "y") {
-      composition_region.bath = true;
-      std::cout << "There is bake in bath (y/n)?" << std::endl;
-      std::cin >> setBake;
-      if (setBake == "y") {
-        composition_region.bath_bake = true;
-      }
+      region.bath = true;
+    } else {
+      region.bath = false;
     }
-    /**
-     * TODO Реализовать заполнение дома
-     **/
-    std::cout << "Enter the number of floors" << std::endl;
-    std::cin >> countFloor;
-    for (int i = 0; i < countFloor; i++) {
-    }
-    region[i] = composition_region;
+  }
+
+  for (int i = 0; i < region.home.room.type_room.size(); i++) {
+    std::cout << i << " " << region.home.room.type_room[i] << " - " << region.home.room.square[i] << std::endl;
   }
 
   return 0;
